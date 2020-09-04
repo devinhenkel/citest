379f53725b294c2a1e43859014e26b939133053f
devinhenkel <devinhenkel@gmail.com>
2020-09-04 11:32:42 -0500

    Emmett is a cool dude

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Emmett is a cool dude.
         </p>
         <a
           className="App-link"
